e8f2e6b9022879b726c2b08063384377d8f5db46
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-09 14:56:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,12 @@ function logWhisper(string) {
 
 function sayHiToGrandma(string) {
   if (string.toLowerCase === string) {
-    return 
+    return "I can't hear you!"
+  } else {
+    if (string.toUpperCase === string) {
+      return "YES INDEED!"
+    } else {
+      
+    }
   }
 }
