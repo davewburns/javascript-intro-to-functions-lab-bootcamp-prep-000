b303dcd8c0a11a86e4188edd98d9bc86aa364d04
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-09 15:01:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,9 @@ function sayHiToGrandma(string) {
     if (string.toUpperCase() === string) {
       return "YES INDEED!"
     } else {
-      if (string.toUpperCase) === string || if (string.)
+      if (string.toUpperCase()) === string || if (string.toLowerCase()) === string) {
+        
+      }
       
     }
   }
