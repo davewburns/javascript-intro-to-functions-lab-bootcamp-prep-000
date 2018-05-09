e1237f51fa233c949fdd3217f3a28324066ab9da
dave.w.burns@gmail.com <dave.w.burns@gmail.com>
2018-05-09 14:47:28 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  //console.log((string).toUpperCase)
+  var lShout = string.toUpperCase
+  console.log(lShout)
+}
+
+function logWhisper(string) {
+  //console.log((string).toLowerCase)
+  var lWhisp = string.toLowerCase
+  console.log(lWhisp)
+}
